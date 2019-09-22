512a8e27cc28ed53c77661259b286697dc4f8cea
ParthShah412 <pinkushah05@gmail.com>
2019-09-22 13:06:30 -0400

    Add files via upload
    File for getting the train/test/val data

---
+++ pyReferDemo.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 01:06:14 2019
+
+@author: pvsha
+"""
+
+from refer import REFER
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+
+data_root = '.\data'  # contains refclef, refcoco, refcoco+, refcocog and images
+dataset = 'refcoco+'
+splitBy = 'unc'
+refer = REFER(data_root, dataset, splitBy)
+
+print('dataset [%s_%s] contains: ' % (dataset, splitBy))
+ref_ids = refer.getRefIds()
+image_ids = refer.getImgIds()
+print('%s expressions for %s refs in %s images.' % (len(refer.Sents), len(ref_ids), len(image_ids)))
+
+print('\nAmong them:')
+if dataset == 'refclef':
+    if splitBy == 'unc':
+        splits = ['train', 'val', 'testA', 'testB', 'testC']
+    else:
+        splits = ['train', 'val', 'test']
+elif dataset == 'refcoco':
+    splits = ['train', 'val', 'test']
+elif dataset == 'refcoco+':
+    splits = ['train', 'val', 'test']
+elif dataset == 'refcocog':
+    splits = ['train', 'val']  # we don't have test split for refcocog right now.
+    
+for split in splits:
+    ref_ids = refer.getRefIds(split=split)
+    print('%s refs are in split [%s].' % (len(ref_ids), split))
+    
+    
+ref_ids = refer.getRefIds()
+ref_id = ref_ids[np.random.randint(0, len(ref_ids))]
+ref = refer.Refs[ref_id]
+print('ref_id [%s] (ann_id [%s])' % (ref_id, refer.refToAnn[ref_id]['id']))
+# show the segmentation of the referred object
+plt.figure()
+refer.showRef(ref, seg_box='box')
+plt.show()
